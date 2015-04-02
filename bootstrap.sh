9279ec189cdb43c805551ca8be0065ae140cad1f
Red Hat Developer CI <n.9086822@gmail.com>
2015-04-02 06:42:09 -0400

    Added bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ set -e
 
 # First stop the Docker container. Needs to be done before the git update so as to stop the current configuration
 cd _docker
-./control.sh
+sudo ./control.sh -s
 
 # Update the repo
 git fetch paulrobinson
