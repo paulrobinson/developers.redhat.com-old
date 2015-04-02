9a0b5e7b13a9da9e467b1f08beace337c3a395c7
Red Hat Developer CI <n.9086822@gmail.com>
2015-04-02 06:38:38 -0400

    Added bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# First stop the Docker container. Needs to be done before the git update so as to stop the current configuration
+cd _docker
+./control.sh
+
+# Update the repo
 git fetch paulrobinson
 git checkout RHD-133
 git pull --rebase paulrobinson RHD-133
