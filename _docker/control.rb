79444c29df5784a34150beb02eb51a445739f6b3
LightGuard <lightguard.jp@gmail.com>
2015-04-15 21:03:02 -0600

    Doing checks in the control script for liveliness
    The control script now checks to see if containers are active and ready
    for data before we move on to the other parts of the build. I need
    someone to test this on OS X to make sure things are working well for
    boot2docker or other ways of running the docker container.

--- _docker/control.rb
+++ _docker/control.rb
@@ -6,6 +6,9 @@ require 'tempfile'
 require 'gpgme'
 require 'yaml'
 require 'digest'
+require 'docker'
+require 'socket'
+require 'timeout'
 
 class Options
   def self.parse(args)
@@ -14,6 +17,10 @@ class Options
     opts_parse = OptionParser.new do |opts|
       opts.banner = 'Usage: control.rb [options]'
       opts.separator 'Specific options:'
+
+      opts.on('-d', '--docker', 'Docker client connection info (i.e. tcp://example.com:1000)') do |d|
+        options[:docker] = d
+      end
 
       opts.on('-r', '--restart', 'Restart the containers') do |r|
         options[:restart] = r
@@ -59,26 +66,71 @@ def modify_env
 end
 
 def execute_docker_compose(cmd, args = [])
-  if cmd == :up
-    Kernel.system 'docker-compose', 'up', '-d'
-  else
-    Kernel.system *['docker-compose', cmd.to_s, *args]
-  end
+  Kernel.system *['docker-compose', cmd.to_s, *args]
 end
 
 def execute_docker(cmd, args = [])
   Kernel.system *['sudo', cmd.to_s, *args]
 end
 
+def options_selected? options
+  (options[:build] || options[:restart] || options[:awestruct][:gen] || options[:awestruct][:preview])
+end
+
+def startup_services
+  execute_docker_compose :up, %w(-d elasticsearch mysql drupalmysql drupal searchisko searchiskoconfigure)
+
+  configure_service = Docker::Container.get('docker_searchiskoconfigure_1')
+
+  while configure_service.info['State']['Running']
+    sleep 5
+    configure_service = Docker::Container.get('docker_searchiskoconfigure_1')
+  end
+
+  # Check to see if Drupal is accepting connections before continuing
+  puts 'Waiting to proceed until Drupal is up'
+  drupal_ip = Docker::Container.get('docker_drupal_1').info["NetworkSettings"]["Ports"]["80/tcp"].first["HostIp"]
+  drupal_port = Docker::Container.get('docker_drupal_1').info["NetworkSettings"]["Ports"]["80/tcp"].first["HostPort"]
+  up = false
+  until up
+    begin
+      Timeout::timeout(1) do
+        begin
+          s = TCPSocket.new(drupal_ip, drupal_port)
+          s.close
+          up = true
+        rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+          # Doesn't matter, just means it's still down
+          sleep 5
+        end
+      end
+    rescue Timeout::Error
+      # We don't really care about this
+    end
+  end
+
+  execute_docker_compose :up, %q(-d --no-recreate awestruct)
+end
+
 options = Options.parse ARGV
 
-puts Options.parse %w(-h) unless options[:build] || options[:restart] || options[:awestruct][:gen] || options[:awestruct][:preview]
+puts Options.parse %w(-h) unless options_selected? options
 
 modify_env
 
+Docker.url = options[:docker] if options[:docker]
+
 if options[:build]
-  FileUtils.cp '../Gemfile', 'ruby' unless Digest::MD5.file('../Gemfile') == Digest::MD5.file('ruby/Gemfile')
-  FileUtils.cp '../Gemfile.lock', 'ruby' unless Digest::MD5.file('../Gemfile.lock') == Digest::MD5.file('ruby/Gemfile.lock')
+  docker_dir = 'ruby'
+
+  parent_gemfile = File.new '../Gemfile'
+  parent_lock = File.new '../Gemfile.lock'
+  docker_gemfile = File.new File.join(docker_dir, 'Gemfile')
+  docker_lock = File.new File.join(docker_dir, 'Gemfile.lock')
+
+  FileUtils.cp parent_gemfile, 'ruby' unless Digest::MD5.file(parent_gemfile) == Digest::MD5.file(docker_gemfile)
+  FileUtils.cp parent_lock, 'ruby' unless Digest::MD5.file(parent_lock) == Digest::MD5.file(docker_lock)
+
   puts 'Building base docker image...'
   execute_docker(:build, ['-t developer.redhat.com/base', './base'])
   puts 'Building base Java docker image...'
@@ -88,14 +140,14 @@ end
 
 if options[:restart]
   execute_docker_compose :kill
-  execute_docker_compose :up
+  startup_services
   execute_docker_compose :ps
 end
 
 if options[:awestruct][:gen]
-  execute_docker_compose :run, %w(awestruct rake clean gen)
+  execute_docker_compose :run, %w(--no-deps awestruct rake clean gen)
 end
 
 if options[:awestruct][:preview]
-  execute_docker_compose :run, %w(awestruct rake clean preview)
+  execute_docker_compose :run, %w(--no-deps awestruct rake clean preview)
 end
